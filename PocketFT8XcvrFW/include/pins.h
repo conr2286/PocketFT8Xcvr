591c0752840f9743db0810472c20577d015253e7
conr2286 <43628320+conr2286@users.noreply.github.com>
2025-07-18 17:19:42 -0700

    Define V3.00 PCB auxilary pins in firmware

--- PocketFT8XcvrFW/include/pins.h
+++ PocketFT8XcvrFW/include/pins.h
@@ -1,3 +1,4 @@
+#pragma once
 /*
 **
 ** NAME
@@ -6,7 +7,7 @@
 ** NOTES
 **  If you change processors, displays, etc, you may need to reevaluate the GPIO pin assignments
 **
-**  The V2.0 boards control the RCV and XMT signals using firmware thru PIN_RCV and PIN_PTT.  
+**  The V2.0 boards control the RCV and XMT signals using firmware thru PIN_RCV and PIN_PTT.
 **  This firmware implementation is compatible with both V1.X and V2.0 boards.
 **
 ** HISTORY
@@ -14,43 +15,51 @@
 **
 **/
 
-//The Adafruit 480x320 display pins
+// The Adafruit 480x320 display pins
 #define PIN_CS 10
 #define PIN_DC 9
 #define PIN_RST 8
 #define PIN_MOSI 11
-#define PIN_DCLK 13  //Teensy 4.1
+#define PIN_DCLK 13  // Teensy 4.1
 #define PIN_MISO 12
 
-//I2C pins
-#define PIN_SCL 19   //Wire
-#define PIN_SDA 18   //Wire
-#define PIN_SCL2 24  //Wire2
-#define PIN_SDA2 25  //Wire2
+// I2C pins
+#define PIN_SCL 19   // Wire
+#define PIN_SDA 18   // Wire
+#define PIN_SCL2 24  // Wire2
+#define PIN_SDA2 25  // Wire2
 
-//I2C bus definitions for the Wire objects
-#define WIRE_RCV Wire   //The SI4735's private bus
-#define WIRE_ETC Wire2  //Everything (SI5351, MCP342x) else
+// I2C bus definitions for the Wire objects
+#define WIRE_RCV Wire   // The SI4735's private bus
+#define WIRE_ETC Wire2  // Everything (SI5351, MCP342x) else
 
-//Serial bus definitions for optional GPS
+// Serial bus definitions for optional GPS
 #define SerialGPS Serial1
 
-//SI4735 pins
+// SI4735 pins
 #define PIN_RESET 20
 
-//Transmit/Receive pins
-#define PIN_PTT 14  //Teensy 4.1
-#define PIN_RCV 15  //FW implementation of ~PTT (i.e. RECV) in V2.0 boards
+// Transmit/Receive pins
+#define PIN_PTT 14  // Teensy 4.1
+#define PIN_RCV 15  // FW implementation of ~PTT (i.e. RECV) in V2.0 boards
 
-//Adafruit resistive touchscreen pins
+// Adafruit resistive touchscreen pins
 #define PIN_YP 38  // must be an analog pin, use "An" notation!
 #define PIN_XM 37  // must be an analog pin, use "An" notation!
 #define PIN_YM 36  // can be a digital pin
 #define PIN_XP 39  // can be a digital pin
 
-//GPS pins (Teensy 4.1 Serial1)
-#define PIN_GPSTX 0  //TX data from GPS
-#define PIN_GPSRX 1  //RX data from GPS
-#define PIN_PPS   2  //PPS pulse
+// GPS pins (Teensy 4.1 Serial1)
+#define PIN_GPSTX 0  // TX data from GPS
+#define PIN_GPSRX 1  // RX data from GPS
+#define PIN_PPS 2    // PPS pulse
 
-//Note:  The audio input pin is apparently hardwired by the Audio Library in input_adc.h to A2
+// Auxilliary pins for future expansion (Require V3.00 PCB)
+#define PIN_AN13 27  // A13
+#define PIN_D27 27   // D27
+#define PIN_D28 28   // D28
+#define PIN_D29 29   // D29
+#define PIN_D30 30   // D30
+#define PIN_D31 31   // D31
+
+// Note:  The audio input pin is apparently hardwired by the Audio Library in input_adc.h to A2
