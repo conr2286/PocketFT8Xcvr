edc2206b5bddd1aca2144dc8ca109633b8712e2e
conr2286 <43628320+conr2286@users.noreply.github.com>
2025-09-21 06:20:56 -0700

    Delete the obsolete AGUI investigation

--- Investigations/AGUI/lib/AGUI/ACoord.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// Define the data type used for x/y pixel coordinates and width/height.  Values need
-// to range anywhere from 0 to the size of the screen.
-typedef unsigned short ACoord;   // Coordinate type
-typedef unsigned short ALength;  // A length (e.g. width or height)
