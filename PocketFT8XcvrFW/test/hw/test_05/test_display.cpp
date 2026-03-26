7a41515310a01f32b066d96fea32a0831f5f9902
conr2286 <43628320+conr2286@users.noreply.github.com>
2026-03-26 15:02:17 -0700

    touchscreen test investigation

--- PocketFT8XcvrFW/test/hw/test_05/test_display.cpp
+++ PocketFT8XcvrFW/test/hw/test_05/test_display.cpp
@@ -44,18 +44,19 @@ TouchScreen ts = TouchScreen(PIN_XDP, PIN_YP, PIN_XM, PIN_YDM, 282);            
  *
  */
 TSPoint pollTouchscreen() {
-    TSPoint rawPoint, mappedPoint;
+    TSPoint pi, pw;
 
-    rawPoint = ts.getPoint();  // Read the screen
+    pi = ts.getPoint();  // Read the screen
 
-    if (rawPoint.z > MINPRESSURE) {                                 // Has screen been touched?
-        mappedPoint.x = map(rawPoint.x, TS_MINX, TS_MAXX, 0, 480);  // Map to resistance to screen coordinate
-        mappedPoint.y = map(rawPoint.y, TS_MINY, TS_MAXY, 0, 320);
-        mappedPoint.z = rawPoint.z;
+    if (pi.z > MINPRESSURE) {  // Has screen been touched?
+        Serial.printf("ADC = (%d,%d,%d)\n", pi.x, pi.y, pi.z);
+        pw.x = map(pi.x, TS_MINX, TS_MAXX, 0, 480);  // Map to resistance to screen coordinate
+        pw.y = map(pi.y, TS_MINY, TS_MAXY, 0, 320);
+        pw.z = pi.z;
     } else {
-        mappedPoint.z = 0;  // Invalid point (no touchpoint available)
+        pw.z = 0;  // Invalid point (no touchpoint available)
     }
-    return mappedPoint;
+    return pw;
 }
 
 // Unity function invoked prior to each test
@@ -64,24 +65,84 @@ void setUp(void) {
 
 // Unity function invoked following each test
 void tearDown(void) {
-}
+    // TSPoint p1;
+    // tft.drawString("Touch to continue", 140, 150);
+    // unsigned long dt = 10;       // ms to wait between polls
+    // unsigned long tWait = 0;     // ms we've awaited touch
+    // while (tWait < 10000L) {     // We're willing to wait 10 seconds
+    //     p1 = pollTouchscreen();  // Read touch input, if any
+    //     if (p1.z > 100) break;   // Touch?
+    //     delay(dt);               // Wait before checking again
+    //     tWait += dt;             // Tally wait ms
+    // }
+    // if (p1.z > 100) {
+    //     Serial.printf("Touch = (%d,%d)\n", p1.x, p1.y);
+    // }
+}  // tearDown()
 
 /**
- * @brief Exercise
+ * @brief Draw a rectangle and label the origin and lower-right corner
+ *
+ * @note There are not sensible assertions here because the HX8357 code
+ * doesn't inform us when something fails, so we rely on user input
  */
 void test_rect(void) {
     tft.drawRoundRect(0, 0, 480, 320, 5, HX8357_YELLOW);
-    tft.drawString("(0,0)", 10, 10);
-    tft.drawString("(480,320)", 400, 305);
-    TEST_ASSERT(true);
+    tft.drawPixel(0, 0, HX8357_WHITE);
+    tft.drawString("(0,0)", 5, 8);
+    tft.drawPixel(479, 319, HX8357_WHITE);
+    tft.drawString("(479,319)", 340, 300);
 }  // test_config()
 
 /**
+ * @brief Check touchscreen x-Axis wiring
+ */
+void test_yAxisPins(void) {
+    int ydp;
+
+    // Ground the Y-Axis
+    pinMode(PIN_YDM, OUTPUT);
+    pinMode(PIN_YP, OUTPUT);
+    digitalWrite(PIN_YDM, LOW);
+    digitalWrite(PIN_YP, LOW);
+
+    // Confirm YDP near zero with y-Axis grounded
+    ydp = analogRead(A17);
+    Serial.printf("ydp=%d\n", ydp);
+
+    // Confirm YDP remains near zero with YM floating and YP grounded
+    for (int i = 0; i < 10; i++) {
+        // Float the Y-Axis
+        pinMode(PIN_YDM, INPUT_DISABLE);
+
+        // Check YDP
+        ydp = analogRead(A17);
+        Serial.printf("%d: ydp=%d\n", i, ydp);
+
+        delay(10);
+    }
+
+    // Confirm YDP remains near zero with y-Axis floating
+    for (int i = 0; i < 10; i++) {
+        // Float the Y-Axis
+        pinMode(PIN_YDM, INPUT_DISABLE);
+        pinMode(PIN_YP, INPUT_DISABLE);
+
+        // Check YDP
+        ydp = analogRead(A17);
+        Serial.printf("%d: ydp=%d\n", i, ydp);
+
+        delay(10);
+    }
+}
+
+/*
  * @brief Run all tests in their prescribed order
  * @return Failure/Success indication
  */
 int runUnityTests(void) {
     UNITY_BEGIN();
+    RUN_TEST(test_yAxisPins);
     RUN_TEST(test_rect);
     return UNITY_END();
 }
@@ -95,6 +156,10 @@ void setup() {
     tft.begin(30000000UL, 2000000UL);  // Configure SPI clock speeds
     tft.setRotation(3);                // Configure landscape screen rotation
     tft.fillScreen(HX8357_BLACK);      // Erase the display
+    tft.setTextSize(2);
+
+    // Discard one ADC reading
+    TSPoint foo = ts.getPoint();
 
     // Run the tests
     runUnityTests();
